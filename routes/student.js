6f36973df97b427041fa804b830812a6568c4932
ntvinh <btranmakemewonder@gmail.com>
2020-08-03 00:18:43 +0700

    Convert json object to array

--- routes/student.js
+++ routes/student.js
@@ -5,35 +5,47 @@ const studentRepo = require('../models/studentRepo');
 router.get('/', (req, res) =>{
   	studentRepo.loadAll().then(rows => {
 		//console.log(rows.recordset[0].Ten);
-		return res.status(200).json(rows);
+		return res.status(200).json(rows.recordset);
 	})
 })
 router.get('/class/:id', (req, res) =>{
 	studentRepo.loadStudentInClass(req.params.id).then(rows => {
-		return res.status(200).json(rows);
+		return res.status(200).json(rows.recordset);
 	})
 })
 router.get('/single/:id', (req, res) =>{
 	studentRepo.singleStudent(req.params.id).then(rows => {
-		return res.status(200).json(rows);
+		return res.status(200).json(rows.recordset);
 	})
 })
 router.get('/score/:id', (req, res) =>{
 	studentRepo.singleScore(req.params.id, req.query.sem, req.query.year).then(rows => {
-		//console.log(rows.recordset[0].Ten);
-		return res.status(200).json(rows);
+		let i = 0, records = []
+		for (i; i<rows.recordset.length; i++){
+			let record = {
+				StudentId: rows.recordset[i].StudentId,
+				SubjectName: rows.recordset[i].SubjectName,
+				StudentId: rows.recordset[i].StudentId,
+				Test15: [rows.recordset[i].Test15No1, rows.recordset[i].Test15No2, rows.recordset[i].Test15No3],
+				Test45: [rows.recordset[i].Test45No1, rows.recordset[i].Test45No2],
+				TestFinal: rows.recordset[i].TestFinal,
+				Final: rows.recordset[i].Final
+			}
+			records[i] = record;
+		}
+		return res.status(200).json(records);
 	})
 })
 router.get('/schedule/:id', (req, res) =>{
 	studentRepo.singleTimeTable(req.params.id).then(rows => {
 		//console.log(rows.recordset[0].Ten);
-		return res.status(200).json(rows);
+		return res.status(200).json(rows.recordset);
 	})
 })
 router.get('/noti/:id', (req, res) =>{
 	studentRepo.singleNoti(req.params.id).then(rows => {
 		//console.log(rows.recordset[0].Ten);
-		return res.status(200).json(rows);
+		return res.status(200).json(rows.recordset);
 	})
 })
 
@@ -48,7 +60,7 @@ router.post('/add', (req, res) =>{
 	studentRepo.add(student).then(result => {
 		studentRepo.singleStudent(student.StudentId).then(rows => {
 			// console.log(rows.recordset[0].Ten)
-			return res.status(200).json(rows);
+			return res.status(200).json(rows.recordset);
 	    })
 	})
 })
