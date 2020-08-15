38367190df9ac9fe36c91eb83fc82edf114ab38e
ntvinh <btranmakemewonder@gmail.com>
2020-08-15 22:33:42 +0700

    teacher go on 7..

--- routes/teacher.js
+++ routes/teacher.js
@@ -26,6 +26,8 @@ router.get('/listclass/:id', jwtt.tokenVerify,(req, res) =>{
 	})
 });
 
+
+
 router.get('/schedule/:id', jwtt.tokenVerify,(req, res) =>{
 	teacherRepo.schedule(req.params.id, req.query.sem, req.query.year).then(rows => {
 		if(typeof rows.recordset === 'undefined' || !rows.recordset.length){
@@ -43,31 +45,43 @@ router.get('/class/:id', jwtt.tokenVerify,(req, res) =>{
 		return res.status(200).json(rows.recordset);
 	})
 })
-// router.get('/score/:id', jwtt.tokenVerify,(req, res) =>{
-// 	scoreRepo.singleScore(req.params.id, req.query.sem, req.query.year).then(rows => {
-// 		let i = 0, records = []
-// 		if(typeof rows.recordset === 'undefined' || !rows.recordset.length){
-// 			return res.status(403).json({
-// 				success: 0,
-// 				message: "Unavailable score!"
-// 			});
-// 		}else{
-// 			for (i; i<rows.recordset.length; i++){
-// 				let record = {
-// 					StudentId: rows.recordset[i].StudentId,
-// 					SubjectName: rows.recordset[i].SubjectName,
-// 					StudentId: rows.recordset[i].StudentId,
-// 					Test15: [rows.recordset[i].Test15No1, rows.recordset[i].Test15No2, rows.recordset[i].Test15No3],
-// 					Test45: [rows.recordset[i].Test45No1, rows.recordset[i].Test45No2],
-// 					TestFinal: rows.recordset[i].TestFinal,
-// 					Final: rows.recordset[i].Final
-// 				}
-// 				records[i] = record;
-// 			}
-// 			return res.status(200).json(records);
-// 		}
-// 	})
-// })
+
+router.get('/listscore/:id', jwtt.tokenVerify,(req, res) =>{
+	scoreRepo.listStudentAndScore(req.params.id, req.query.sem, req.query.year).then(rows => {
+		let i = 0, records = []
+		if(typeof rows.recordset === 'undefined' || !rows.recordset.length){
+			return res.status(403).json({
+				success: 0,
+				message: "Unavailable score!"
+			});
+		}else{
+			for (i; i<rows.recordset.length; i++){
+				let record = {
+					ClassId: rows.recordset[i].ClassId,
+					StudentId: rows.recordset[i].StudentId,
+					LastName: rows.recordset[i].LastName,
+					MiddleName: rows.recordset[i].MiddleName,
+					FirstName: rows.recordset[i].FirstName,
+					Email: rows.recordset[i].email,
+					Birthday: rows.recordset[i].Birthday,
+					Address: rows.recordset[i].Address,
+					IsMale: rows.recordset[i].IsMale,
+					PhoneNumber: rows.recordset[i].PhoneNumber,
+					StudentId: rows.recordset[i].StudentId,
+					StudentId: rows.recordset[i].StudentId,
+					Test15: [rows.recordset[i].Test15No1, rows.recordset[i].Test15No2, rows.recordset[i].Test15No3],
+					Test45: [rows.recordset[i].Test45No1, rows.recordset[i].Test45No2],
+					TestFinal: rows.recordset[i].TestFinal,
+					Final: rows.recordset[i].Final,
+					Semester: rows.recordset[i].Semester,
+					AcademicYear: rows.recordset[i].AcademicYear
+				}
+				records[i] = record;
+			}
+			return res.status(200).json(records);
+		}
+	})
+})
 
 // router.get('/noti/:id', jwtt.tokenVerify,(req, res) =>{
 // 	studentRepo.singleNoti(req.params.id).then(rows => {
